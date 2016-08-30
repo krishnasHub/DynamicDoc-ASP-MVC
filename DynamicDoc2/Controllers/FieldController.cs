5ed44934c6a85ae094dddc064a8a98f8f56899d8
sweta <bajaj.sw@husky.neu.edu>
2016-08-30 03:21:01 -0400

    modified fieldController

--- DynamicDoc2/Controllers/FieldController.cs
+++ DynamicDoc2/Controllers/FieldController.cs
@@ -19,7 +19,7 @@ namespace DynamicDoc2.Controllers
 
         public JsonResult CreateNewField(string fieldName, int fieldID)
         {
-            return 
+            return null;
         }
     }
 }
